70466de28f25549ac2ac46dd441e175f8d97a2a3
Sabrina Baccam <sabrinabaccam@gmail.com>
2022-10-27 20:06:01 -0500

    done

--- data_presenter.py
+++ data_presenter.py
@@ -1,25 +1,25 @@
-total_choc = 0
-total_van = 0
-total_sberry = 0
+total = 0
 
 open_file = open("CupcakeInvoices.csv")
 for line in open_file:
-    line = line.strip() # gets rid of white space
-    values = line.split(',') # splits by commas
-    for value in values:
-        if value == "Chocolate":
-            total_choc += 1
-        elif value == "Vanilla":
-            total_van += 1
-        elif value == "Strawberry":
-            total_sberry += 1
-    print(values[0:2]) # prints first and last names, 2 is not inclusive
+    line = line.strip()
+    # print(line)
+
+    values = line.split(',')
+    # print(values[2])
+
+    quantity = float(values[3])
+    price = float(values[4])
+    invoice = quantity * price
+    print(invoice)
+
+    total += invoice
 open_file.close()
 
-print(total_choc)
-print(total_van)
-print(total_sberry)
+print("TOTAL: $" + str(total))
 
+
+# other way to open file
 # with open("CupcakeInvoices.csv") as file:
 #     df = file.read()
 #     print(df)
--- starter.py
+++ starter.py
@@ -101,29 +101,3 @@ friends.insert(2, "Momo")
 # Problem 15
 # Remove the last item in the array (try to think about how you can do this dynamically, meaning, if the array contents were to change, your code would still work).
 friends.pop()
-
-total_choc = 0
-total_van = 0
-total_sberry = 0
-
-open_file = open("CupcakeInvoices.csv")
-for line in open_file:
-    line = line.strip() # gets rid of white space
-    values = line.split(',') # splits by commas
-    for value in values:
-        if value == "Chocolate":
-            total_choc += 1
-        elif value == "Vanilla":
-            total_van += 1
-        elif value == "Strawberry":
-            total_sberry += 1
-    print(values[0:2]) # prints first and last names, 2 is not inclusive
-open_file.close()
-
-print(total_choc)
-print(total_van)
-print(total_sberry)
-
-# with open("CupcakeInvoices.csv") as file:
-#     df = file.read()
-#     print(df)
